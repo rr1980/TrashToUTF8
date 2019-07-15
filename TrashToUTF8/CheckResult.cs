3aa7d7662ec2195d7bc80a33e9b16eea3d8f34a8
rr1980 <rener1980@googlemail.com>
2019-07-15 11:04:57 +0200

    finished

---
+++ TrashToUTF8/CheckResult.cs
@@ -0,0 +1,11 @@
+﻿namespace TrashToUTF8
+{
+    public partial class Parser
+    {
+        class CheckResult
+        {
+            public bool Found { get; set; }
+            public string FoundChar { get; set; }
+        }
+    }
+}
---
+++ TrashToUTF8/Config.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TrashToUTF8
+{
+    public static class Config
+    {
+        public static string SourcePath { get; set; } = @"D:\Projekte\TrashToUTF8\TrashToUTF8\SB\Dirty.sql";
+        public static string TargetPath { get; set; } = @"D:\Projekte\TrashToUTF8\TrashToUTF8\SB\Results\Clean.sql";
+        public static string LogPath { get; set; } = @"D:\Projekte\TrashToUTF8\TrashToUTF8\SB\Results\Log.txt";
+        public static string LogFailPath { get; set; } = @"D:\Projekte\TrashToUTF8\TrashToUTF8\SB\Results\LogFail.txt";
+
+        public static Encoding SourceEncoding { get; set; } = Encoding.GetEncoding("windows-1252");
+        public static Encoding TargetEncoding { get; set; } = Encoding.UTF8;
+
+        public static Regex Regex { get; set; } = new Regex("'([^0-9]+)'", RegexOptions.Multiline);
+
+        public static List<string> SearchChars = new List<string> {
+            "Å",
+            "Ã",
+            "©",
+            "º",
+            "Ã",
+            "‡",
+            "™",
+            "…",
+            "Å",
+            "¾",
+            "†",
+            "»",
+            "°",
+            "Ñ",
+        };
+
+        public static List<string> BlackChars = new List<string> {
+            "ￅ",
+            "�",
+            "¬",
+            //"¾",
+        };
+    }
+}
